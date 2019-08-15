b57f8c3a6f51604994c28db3e7b49dd0984e8eb5
Cody Alton <jcalton88@gmail.com>
2019-08-15 14:02:17 -0400

    Add files missing in rename from src to services

---
+++ services/shared/models/github-repo.ts
@@ -0,0 +1,10 @@
+export interface IGithubRepo {
+    id: number;
+    name: string;
+    full_name: string;
+    private: boolean;
+    description: string;
+    issues_url: string;
+    open_issues_count: number;
+}
+
